9566131ef4bcc7240f16c6d26b2633724ab47b01
Wenbin Hou <catchyrime@fastmail.com>
2017-09-06 19:29:51 +0800

    Rewrite: now env-reload works quite well

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 """
 
 This file should NOT be directly used by users.
@@ -36,222 +37,258 @@ import codecs
 import string
 import re
 
-#
-# Global variables
-#
+g_StrExpandPattern = re.compile(r"""
+    \$ (?: (?P<dollar>\$)                           # tuple index: 0
+         | (?P<raw>[A-Za-z_][A-Za-z0-9_]*)          # tuple index: 1
+         | {(?P<brace>\.?[A-Za-z_][A-Za-z0-9_.]*)}  # tuple index: 2
+         | (?P<invalid>.|$)                         # tuple index: 3
+         )""", re.VERBOSE | re.DOTALL)
+
+
 g_EnvCoufPath = ""
 g_OutPath = ""
 g_OutMetaPath = ""
-g_Home = ""
 
 
-def env_reload():
+def expand_string_one_impl(template, known_map):
 
-    """
-    Load or reload environment variables defined in g_EnvCoufPath.
-    The output file (g_OutPath) is saved in environment variable SYSENV_CURRENT_OUTPUT.
-    This variable will be defined (for the first time - loading) or changed (for the following time - reloading).
-    The output file MUST be placed in a tmpfs (eg. /dev/shm) as it will NOT be deleted automatically.
+    # Helper function for .sub()
+    def convert(match):
+        # Check the most common path first.
+        name = match.group("raw") or match.group("brace")
+        if name is not None:
+            return known_map[name] if (name in known_map) else ""
+        elif match.group("dollar") is not None:
+            return "$"
+        elif match.group("invalid") is not None:
+            raise ValueError("Unrecognized variable definition in %s" % template)
 
-    The following features are provided:
-      - Use '~' at start as short for '$HOME'
-      - Use $XXX as reference to other environment variables, which will be expanded
-    """
+    return g_StrExpandPattern.sub(convert, template)
 
-    def read_conf_file():
-        result = defaultdict(list)
-        current_list = None
-        patten = re.compile(r'\[(?: \t)*([A-Za-z_][A-Za-z0-9_]*|\.install_root)(?: \t)*\]')
-        conf = codecs.open(g_EnvCoufPath, 'r', 'utf-8')
-        for line in conf:
+
+def query_expand_string_vars(template):
+    found = g_StrExpandPattern.findall(template)
+    result = set()
+    for tuple4 in found:  # tuple4 = (P<dollar>, P<raw>, P<brace>, P<invalid>)
+        if len(tuple4[1]) > 0:      # P<raw>
+            result.add(tuple4[1])
+        elif len(tuple4[2]) > 0:    # P<brace>
+            result.add(tuple4[2])
+        elif len(tuple4[3]) > 0:    # P<invalid>
+            raise ValueError("Unrecognized variable definition in %s" % template)
+    return list(result)
+
+
+def expand_string_one(template, var_list, known_maps):
+    #
+    # `known_maps` looks like:
+    # {
+    #   "var1": [ "value11", "value12" ],
+    #   "var2": [ "value21", "value22" ],
+    #   ...
+    # }
+    #
+    # `template` looks like:
+    #   "This is $$, $var1 $var2 $var_non_existent"
+    #
+    # Returns a list of string: the expanded results. (Cartesian product)
+    #
+    for var in var_list:
+        assert (var in known_maps)
+        assert (len(known_maps[var]) >= 1)
+
+    result = []
+
+    dicts = [[], []]
+    which = 0
+    for var in var_list:
+        other = 1 - which
+        dicts[which] = []
+        if len(dicts[other]) == 0:
+            for s in known_maps[var]:
+                dicts[which].append({var: s})
+        else:
+            for d in dicts[other]:
+                for s in known_maps[var]:
+                    newd = copy.deepcopy(d)
+                    assert (var not in newd)
+                    newd[var] = s
+                    dicts[which].append(newd)
+        which = other
+
+    if len(dicts[1 - which]) == 0:
+        result.append(expand_string_one_impl(template, {}))
+    else:
+        for var_dict in dicts[1 - which]:
+            # print(var_dict)
+            result.append(expand_string_one_impl(template, var_dict))
+
+    # print(result)
+    return result
+
+
+def restore_env():
+    unset_vars = []
+    result = copy.deepcopy(os.environ)
+    if not os.path.isfile(g_OutMetaPath):
+        return result, unset_vars
+
+    # Meta file structure:
+    # {
+    #   "conf": {
+    #     "env_name1": [ "v11", "v12", ... ],
+    #     "env_name2": [ "v21", "v22", ... ],
+    #     ...
+    #   },
+    #   "create": [
+    #     "env_nameX", "env_nameY", ...
+    #   ]
+    # }
+    with codecs.open(g_OutMetaPath, "r", "utf-8") as metaFile:
+        meta = json.load(metaFile)
+        old_conf = meta["conf"]
+        old_create = meta["create"]
+
+    for env_name in old_conf:
+        if env_name not in result:
+            continue
+        for env_value in old_conf[env_name]:
+            if result[env_name] == env_value:
+                result[env_name] = ""
+            elif result[env_name].startswith(env_value + ":"):
+                result[env_name] = result[env_name][len(env_value) + 1:]
+            elif result[env_name].endswith(":" + env_value):
+                result[env_name] = result[env_name][:-(len(env_value) + 1)]
+            else:
+                result[env_name] = result[env_name].replace(":" + env_value + ":", ":")
+
+        # If the variable is empty now, and it's created by the last reload,
+        # remove this variable.
+        if (len(result[env_name]) == 0) and (env_name in old_create):
+            del result[env_name]
+            unset_vars.append(env_name)
+
+    return result, unset_vars
+
+
+def expand_strings(curr_conf):
+    (curr_env, unset_vars) = restore_env()
+    expand_as = {}
+    status_done = {}
+
+    result = {}
+    created_vars = set()
+
+    def inner_expand(name):
+        if name in status_done:
+            if status_done[name]:
+                return
+            else:
+                raise RuntimeError("Recursive environment variable dependency in '%s'" % name)
+        status_done[name] = False
+        expand_as[name] = []
+
+        if name in curr_conf:
+            if name not in curr_env:
+                created_vars.add(name)
+
+            result[name] = []
+            for line in curr_conf[name]:
+                # print(line)
+                var_list = query_expand_string_vars(line)
+                for match_name in var_list:
+                    inner_expand(match_name)
+                expanded = expand_string_one(line, var_list, expand_as)
+                result[name] += expanded
+                expand_as[name] += expanded
+                # print()
+
+        # Append existing system environments
+        if name in curr_env:
+            expand_as[name] += filter(lambda s: len(s) > 0, curr_env[name].split(':'))
+
+        # If the variable doesn't exist at last, specify an empty string.
+        # Just as if the system had defined it an empty string.
+        if len(expand_as[name]) == 0:
+            expand_as[name].append("")
+
+        status_done[name] = True
+
+    for env_name in curr_conf:
+        inner_expand(env_name)
+
+    return curr_env, result, list(created_vars), unset_vars
+
+
+def read_conf_file():
+    result = defaultdict(list)
+    current_lists = None
+    patten = re.compile(r'\s*(\.?[A-Za-z_][A-Za-z0-9_]*)\s*')
+    with codecs.open(g_EnvCoufPath, 'r', 'utf-8') as conffile:
+        for line in conffile:
             line = line.strip("\r\n\t ")
 
             # Skip empty lines & comments
             if len(line) == 0 or line.startswith('#'):
                 continue
 
-            if line[0] == '[':  # Now starting a new section
-                match = patten.match(line)
-                if match:
-                    current_list = result[match.group(1)]
-                else:
+            if line[0] == '[' and line[-1] == ']':  # Now starting a new section
+                current_lists = map(lambda name: result[name], patten.findall(line[1:-1]))
+                if len(current_lists) == 0:
                     raise ValueError("The environment name '%s' is invalid" % line)
             else:
                 # Check that only legal characters are in this line
-                if ':' in line or '\\' in line:
+                if '\\' in line:
                     raise ValueError("The following line contains invalid character(s): %s" % line)
 
-                # Expand head '~' to $HOME
+                # Replace head '~' with $HOME
                 if line[0] == '~':
                     if g_Home is None:
                         raise ValueError("'~' is used, but $HOME is not set")
                     line = "$HOME" + line[1:]
 
-                current_list.append(line)
-        conf.close()
+                for current_list in current_lists:
+                    current_list.append(line)
 
-        return result
-
-    def expand_env_var(ddict_raw, dict_result):
-
-        pattern = re.compile(r'\$[A-Za-z_][A-Za-z0-9_]*|\$\{[A-Za-z_][A-Za-z0-9_]*\}')
-
-        # env[name] is a list := [ <env.conf expanded> ] + [ <system defined> ]
-        env = {}
-        status_done = {}
-
-        def env_flatten(line, varlist, known_maps):
-            for var in varlist:
-                assert (var in known_maps)
-                assert (len(known_maps[var]) >= 1)
-
-            result = []
-            template = string.Template(line)
-
-            dicts = [[], []]
-            which = 0
-            for var in varlist:
-                other = 1 - which
-                dicts[which] = []
-                if len(dicts[other]) == 0:
-                    for s in known_maps[var]:
-                        dicts[which].append({var: s})
-                else:
-                    for d in dicts[other]:
-                        for s in known_maps[var]:
-                            newd = copy.deepcopy(d)
-                            assert (var not in newd)
-                            newd[var] = s
-                            dicts[which].append(newd)
-                which = other
-
-            if len(dicts[1 - which]) == 0:
-                result.append(template.substitute({}))
-            else:
-                for var_dict in dicts[1 - which]:
-                    # print(var_dict)
-                    result.append(template.substitute(var_dict))
-
-            # print(result)
-            return result
-
-        def inner_expand(name):
-            if name in status_done:
-                if status_done[name]:
-                    return
-                else:
-                    raise RuntimeError("Recursive environment variable dependency in '%s'" % name)
-            status_done[name] = False
-            env[name] = []
-
-            if name in ddict_raw:
-                dict_result[name] = []
-                for line in ddict_raw[name]:
-                    # print(line)
-                    matches = pattern.findall(line)
-                    matches = map(lambda s: s[2:-1] if s[1] == '{' else s[1:], matches)
-                    matches = list(set(matches)) # Unique
-                    for match_name in matches:
-                        inner_expand(match_name)
-                    expanded = env_flatten(line, matches, env)
-                    dict_result[name] += expanded
-                    env[name] += expanded
-                    # print()
-
-            # Append existing system environments
-            if name in curr_env:
-                env[name] += filter(lambda s: len(s) > 0, curr_env[name].split(':'))
-
-            # If the environment doesn't exist at last, specify an empty string.
-            # Just as if the system had defined it an empty string.
-            if len(env[name]) == 0:
-                env[name].append("")
-
-            status_done[name] = True
-
-        #
-        # Special patch: for ".install_root" section
-        #
-        DUMMY_NAME = "_SYSENV_INSTALL_ROOT_hAc5pPe9ECE7v9hDcMqrlB349"
-        if ".install_root" in ddict_raw and len(ddict_raw[".install_root"]) > 0:
-            ddict_raw[DUMMY_NAME] = ddict_raw[".install_root"]
-            ddict_raw["C_INCLUDE_PATH"].append("$%s/include" % DUMMY_NAME)
-            ddict_raw["CPLUS_INCLUDE_PATH"].append("$%s/include" % DUMMY_NAME)
-            ddict_raw["LIBRARY_PATH"].append("$%s/lib" % DUMMY_NAME)
-            ddict_raw["LD_LIBRARY_PATH"].append("$%s/lib" % DUMMY_NAME)
-            ddict_raw["PKG_CONFIG_PATH"].append("$%s/lib/pkgconfig" % DUMMY_NAME)
-            ddict_raw["PATH"].append("$%s/bin" % DUMMY_NAME)
-            ddict_raw["MANPATH"].append("$%s/share/man" % DUMMY_NAME)
-
-        for key in ddict_raw:
-            inner_expand(key)
-
-        if ".install_root" in ddict_raw and len(ddict_raw[".install_root"]) > 0:
-            del ddict_raw[DUMMY_NAME]
-            del dict_result[DUMMY_NAME]
+    return result
 
 
-    #
-    # Remove original loaded environment variables
-    #
-    unset_env = []
-    curr_env = copy.deepcopy(os.environ)
-    if os.path.isfile(g_OutMetaPath):
-        with codecs.open(g_OutMetaPath, 'r', 'utf-8') as metafile:
-            org_dict = json.load(metafile)
-        # print(org_dict)
-        for env_name in org_dict:
-            if env_name not in curr_env:
-                continue
-            for env_value in org_dict[env_name]:
-                if curr_env[env_name] == env_value:
-                    curr_env[env_name] = ""
-                elif curr_env[env_name].startswith(env_value + ":"):
-                    curr_env[env_name] = curr_env[env_name][len(env_value) + 1:]
-                elif curr_env[env_name].endswith(":" + env_value):
-                    curr_env[env_name] = curr_env[env_name][:-(len(env_value) + 1)]
-                else:
-                    curr_env[env_name] = curr_env[env_name].replace(":" + env_value + ":", ":")
-            if len(curr_env[env_name]) == 0:
-                unset_env.append(env_name)
-                del curr_env[env_name]
+def env_reload():
+    curr_conf = read_conf_file()
+    (curr_env, new_conf, created_vars, unset_vars) = expand_strings(curr_conf)
 
-    #
-    # Main codes
-    #
-    env_ddict_raw = read_conf_file()
-
-    #
-    # Expand environ variables ($XXX) in conf file
-    #
-    env_dict = {}
-    expand_env_var(env_ddict_raw, env_dict)
-
-    # Print the result to meta
-    with codecs.open(g_OutMetaPath, 'w', 'utf-8') as metafile:
-        json.dump(env_dict, metafile)
+    # Serialize to meta file
+    with codecs.open(g_OutMetaPath, "w", "utf-8") as metafile:
+        json.dump({"conf": new_conf, "create": created_vars}, metafile)
 
     # Print the result
-    with codecs.open(g_OutPath, 'w', 'utf-8') as outfile:
-        for env_name in env_dict:
-            if env_name.startswith('.'):
+    with codecs.open(g_OutPath, "w", "utf-8") as outfile:
+        for env_name in new_conf:
+            if env_name.startswith("."):
                 continue
-            if env_name in unset_env:
-                unset_env.remove(env_name)
+            if env_name in unset_vars:
+                unset_vars.remove(env_name)
 
             if env_name in curr_env:
-                outline = string.join(env_dict[env_name] + [curr_env[env_name]], ':')
+                outline = string.join(new_conf[env_name] + [curr_env[env_name]], ':')
             else:
-                outline = string.join(env_dict[env_name], ':')
-            outline = outline.replace('"', '\\"')
+                outline = string.join(new_conf[env_name], ':')
+            outline = outline.replace('"', '\\"').replace('$', '\\$')
             outfile.write('export %s="%s"\n' % (env_name, outline))
 
-        for env_name in unset_env:
+        for env_name in unset_vars:
             outfile.write('unset "%s"\n' % env_name)
 
-
-def env_mpi_select():
-    pass
+        # These are previously modified variables, but now they are NOT in env.conf file.
+        # Restore them to the original values
+        for env_name in curr_env:
+            if (env_name in new_conf) or (env_name in unset_vars):
+                continue
+            if curr_env[env_name] == os.environ[env_name]:
+                continue
+            outline = curr_env[env_name]
+            outline = outline.replace('"', '\\"').replace('$', '\\$')
+            outfile.write('export %s="%s"\n' % (env_name, outline))
 
 
 if __name__ == "__main__":
@@ -268,6 +305,8 @@ if __name__ == "__main__":
     if sys.argv[1] == 'reload':
         env_reload()
     elif sys.argv[1] == 'mpi-select':
-        env_mpi_select()
+        #env_mpi_select()
+        pass
     else:
         raise SyntaxError("[FATAL] Action '%s' is not defined" % sys.argv[1])
+
--- env.bashrc
+++ env.bashrc
@@ -1,16 +1,28 @@
 
+if [ $_ == $0 ]; then
+    echo "[ERROR] sysenv: $(realpath "$0") must be sourced but not invoked"
+    exit 1
+fi
+
 if [ -z "$SYSENV_OUTPUT_DIRECTORY" ]; then
 export SYSENV_OUTPUT_DIRECTORY="/dev/shm/sysenv/${RANDOM}${RANDOM}${RANDOM}${RANDOM}"
 
 env-reload() {
+
+    if [ ! -r "$HOME/env.conf" ]; then
+        echo "[ERROR] sysenv: \$HOME/env.conf is not found"
+        return 1
+    fi
+
     if [ ! -d "$SYSENV_OUTPUT_DIRECTORY" ]; then
         mkdir -p "$SYSENV_OUTPUT_DIRECTORY"
     fi
 
-    python /sysenv/core.py reload ~/env.conf "$SYSENV_OUTPUT_DIRECTORY/bashrc"
-
-    #cat "$SYSENV_OUTPUT_DIRECTORY/bashrc"
-    source "$SYSENV_OUTPUT_DIRECTORY/bashrc"
+    python "$(dirname "${BASH_SOURCE[0]}")/core.py" reload "$HOME/env.conf" "$SYSENV_OUTPUT_DIRECTORY/bashrc"
+    if [ $? == 0 ]; then
+        #cat "$SYSENV_OUTPUT_DIRECTORY/bashrc"
+        source "$SYSENV_OUTPUT_DIRECTORY/bashrc"
+    fi
     rm "$SYSENV_OUTPUT_DIRECTORY/bashrc"
 }
 
